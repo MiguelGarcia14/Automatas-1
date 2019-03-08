b39cb0c7c5fc14a2e29267e48e8302a852ac2510
campu <miguelagc631@gmail.com>
2019-03-08 12:42:31 -0600

    Se agrego otra nueva clase Se agrego otro nuevo mensaje de consola

--- src/Principal.java
+++ src/Principal.java
@@ -10,5 +10,7 @@ public class Principal
         System.out.println("Tercera prueba");
 
         JOptionPane.showMessageDialog(null, "Cuarta prueba!");
+        
+        System.out.println("Haciendo las ultimas pruebas");
     }
 }
